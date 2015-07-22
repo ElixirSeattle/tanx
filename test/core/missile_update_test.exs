8b5d09295eac8ff020bd45ad608fad54e3194cba
Kyle Rippey <kyle@avvo.com>
2015-07-21 19:53:41 -0700

    Refactored the missile tests, slightly.

--- test/core/missile_update_test.exs
+++ test/core/missile_update_test.exs
@@ -5,11 +5,11 @@ defmodule Tanx.MissileUpdateTest do
     {:ok, game} = Tanx.Core.Game.start_link(clock_interval: nil)
     game |> Tanx.Core.Game.manual_clock_tick(1000)
     {:ok, player} = game |> Tanx.Core.Game.connect(name: "Ben")
+    :ok = player |> Tanx.Core.Player.new_tank()
     {:ok, game: game, player: player}
   end
 
   test "missile moves at constant velocity", %{game: game, player: player} do
-    :ok = player |> Tanx.Core.Player.new_tank()
     :ok = player |> Tanx.Core.Player.new_missile()
 
     game |> Tanx.Core.Game.manual_clock_tick(2000)
@@ -17,15 +17,21 @@ defmodule Tanx.MissileUpdateTest do
   end
 
   test "missile moves on an angle with constant velocity", %{game: game, player: player} do
-    :ok = player |> Tanx.Core.Player.new_tank()
     :ok = player |> Tanx.Core.Player.control_tank(:right, true)
-    
+
     game |> Tanx.Core.Game.manual_clock_tick(2000)
     :ok = player |> Tanx.Core.Player.new_missile()
-    game |> Tanx.Core.Game.manual_clock_tick(4000)
-    _check_missile(player, -0.8322936730942848, -1.8185948536513634, -2.0) #Need to double check these numbers
-  end
 
+    game |> Tanx.Core.Game.manual_clock_tick(2000)
+    
+    # Missile has been created at the origin
+    _check_missile(player, 0, 0, -2.0)
+
+    game |> Tanx.Core.Game.manual_clock_tick(4000)
+
+    # Missile should have changed position, but maintained the same angle.
+    _check_missile(player, -0.8322936730942848, -1.8185948536513634, -2.0)
+  end
 
   # Utils
 
