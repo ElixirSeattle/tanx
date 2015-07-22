34d13b5a0714cc81fd308788c6eaed6d74d98780
Daniel Azuma <dazuma@gmail.com>
2015-07-21 22:51:04 -0700

    Missile needs to be a GenServer, and doesn't need angular velocity

--- lib/tanx/core/missile.ex
+++ lib/tanx/core/missile.ex
@@ -1,18 +1,18 @@
-defmodule Tanx.Core.Missile do 
+defmodule Tanx.Core.Missile do
 
-  defmodule State do 
-    defstruct player: nil, x: 0.0, y: 0.0, heading: 0.0, av: 0.0, v: 10.0
+  defmodule State do
+    defstruct player: nil, x: 0.0, y: 0.0, heading: 0.0, v: 10.0
   end
 
   ############
   #Missile API
 
   #Called by "Arena Objects" process.
-  def start_link(player, params) do 
+  def start_link(player, params) do
     GenServer.start_link(__MODULE__, {player, params})
   end
 
-  #This api currently isn't used as the :update cast is called directly. 
+  #This api currently isn't used as the :update cast is called directly.
   def update(pid, last_time, time, updater) do
     GenServer.cast(pid, {:update, last_time, time, updater})
   end
@@ -29,19 +29,20 @@ defmodule Tanx.Core.Missile do
   #########################
   #GenServer Implementation
 
+  use GenServer
+
   def init({player, {x, y, a}}) do
-    {:ok, %Tanx.Core.Missile.State{player: player, x: x, y: y, heading: a}}  
+    {:ok, %Tanx.Core.Missile.State{player: player, x: x, y: y, heading: a}}
   end
 
-  def handle_cast({:update, last_time, time, updater}, state) do 
+  def handle_cast({:update, last_time, time, updater}, state) do
     dt = max((time - last_time) / 1000, 0.0)
     a = state.heading
     v = state.v
-    na = a + state.av * dt
-    nx = state.x + v * dt * :math.cos(na)
-    ny = state.y + v * dt * :math.sin(na)
-    state = %State{state | x: nx, y: ny, heading: na}
-    update = %Tanx.Core.Updates.MoveMissile{player: state.player, x: nx, y: ny, heading: na}
+    nx = state.x + v * dt * :math.cos(a)
+    ny = state.y + v * dt * :math.sin(a)
+    state = %State{state | x: nx, y: ny}
+    update = %Tanx.Core.Updates.MoveMissile{player: state.player, x: nx, y: ny, heading: a}
     GenServer.cast(updater, {:update_reply, self, update})
     {:noreply, state}
   end
