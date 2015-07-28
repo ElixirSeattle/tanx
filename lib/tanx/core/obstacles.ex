dbd3f8594d14c896512e566d10053efbe1d52220
Daniel Azuma <dazuma@gmail.com>
2015-07-28 13:00:28 -0700

    Add helper to obstacles module for testing intersection against all walls

--- lib/tanx/core/obstacles.ex
+++ lib/tanx/core/obstacles.ex
@@ -70,21 +70,55 @@ defmodule Tanx.Core.Obstacles do
 
 
   @doc """
-  Given a decomposed wall, and two points representing two locations of a point object,
-  returns either the point of impact on the wall, or nil for no impact.
+  Given a single decomposed wall, and two points representing two locations of a
+  point object, returns either the point of impact on the wall, or nil for no impact.
   """
-  def collision_with_decomposed_wall({_elements, segments}, from, to) do
-    segments |> Enum.find_value(&(segment_intersection(&1, from, to)))
+  def collision_with_decomposed_wall(decomposed_wall, from, to) do
+    decomposed_wall
+      |> wall_collision_as_ratio(from, to)
+      |> ratio_to_point(from, to)
   end
 
 
-  defp segment_intersection({p0, p1}, from, to) do
+  @doc """
+  Given a list of decomposed walls, and two points representing two locations of a
+  point object, returns either the first point of impact on a wall, or nil for no impact.
+  """
+  def collision_with_decomposed_walls(decomposed_walls, from, to) do
+    decomposed_walls
+      |> Enum.map(&(wall_collision_as_ratio(&1, from, to)))
+      |> min_ratio_or_nil
+      |> ratio_to_point(from, to)
+  end
+
+
+  defp ratio_to_point(nil, _from, _to), do: nil
+  defp ratio_to_point(ratio, from, to) do
+    vdiff(to, from) |> vscale(ratio) |> vadd(from)
+  end
+
+
+  defp min_ratio_or_nil(values) do
+    values |> Enum.min_by(fn
+      nil -> 2.0
+      val -> val
+    end)
+  end
+
+
+  defp wall_collision_as_ratio({_elements, segments}, from, to) do
+    segments
+      |> Enum.map(&(segment_intersection_as_ratio(&1, from, to)))
+      |> min_ratio_or_nil
+  end
+
+
+  defp segment_intersection_as_ratio({p0, p1}, from, to) do
     from_mag = cross_magnitude(p0, from, p1)
     to_mag = cross_magnitude(p0, to, p1)
     if from_mag < 0 and to_mag >= 0 and
         cross_magnitude(from, p0, to) >= 0 and cross_magnitude(from, p1, to) <= 0 do
-      ratio = from_mag / (from_mag - to_mag)
-      vdiff(to, from) |> vscale(ratio) |> vadd(from)
+      from_mag / (from_mag - to_mag)
     else
       nil
     end
